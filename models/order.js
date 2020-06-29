375a5307aefddf733f3247cd840c799c4a5b32a1
Francis Kerr <KerrFamily@Franciss-MacBook-Pro.local>
2020-06-29 17:11:35 -0400

    push to github

---
+++ models/order.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { ObjectId } = mongoose.Schema;
+
+const CartItemSchema = new mongoose.Schema(
+  {
+    product: { type: ObjectId, ref: "Product" },
+    name: String,
+    price: Number,
+    count: Number
+  },
+  { timestamps: true }
+);
+
+const CartItem = mongoose.model("CartItem", CartItemSchema);
+
+const OrderSchema = new mongoose.Schema(
+  {
+    products: [CartItemSchema],
+    transaction_id: {},
+    amount: { type: Number },
+    address: String,
+    method: String,
+    status: {
+      type: String,
+      default: "Not processed",
+      enum: ["Not processed", "Processing", "Completed", "Cancelled"] // enum means string objects
+    },
+    updated: Date,
+    user: { type: ObjectId, ref: "User" }
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", OrderSchema);
+
+const shippingSchema = {
+  address: {type: String, required: true},
+  city: {type: String, required: true},
+  state: {type: String, required: true},
+  postalCode: {type: String, required: true},
+  country: {type: String, required: true},
+};
+
+const Shipping = mongoose.model("Shipping", shippingSchema);
+
+module.exports = { Order, CartItem, Shipping };
